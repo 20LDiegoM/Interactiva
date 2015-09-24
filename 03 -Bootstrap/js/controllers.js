9d0117ad41cc34c4d79418d34a4e48e00c9bee2c
Luis Diego Murcia <luisdiego.m20@gmail.com>
2015-09-23 18:54:49 -0600

    Prueba 1

---
+++ 03 -Bootstrap/js/controllers.js
@@ -0,0 +1,10 @@
+var submodule = angular.module('bootstrap.controller', []);
+
+submodule.controller('HomeController', ['#scope', function($scope) {
+
+}])
+
+submodule.controller('ProductsController', [function() {
+
+}]);
+a
