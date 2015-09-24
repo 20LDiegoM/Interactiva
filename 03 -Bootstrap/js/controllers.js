cfd862760238a4b523967fe3e4d9fdc7675a7751
Luis Diego Murcia <luisdiego.m20@gmail.com>
2015-09-23 19:25:01 -0600

    2

--- 03 -Bootstrap/js/controllers.js
+++ 03 -Bootstrap/js/controllers.js
@@ -1,10 +1,24 @@
 var submodule = angular.module('bootstrap.controller', []);
 
 submodule.controller('HomeController', ['#scope', function($scope) {
+  $scope.title = 'Hello World';
+  $scope.promotions = {
+    '001': {
+      name: 'Coca Cola',
+      price: '$2'
+    },
+    '002': {
+      name: 'Pepsi',
+      price: '$3'
+    },
+    '003': {
+      name: 'Red Bull',
+      price: '$4'
+    }
+  };
 
 }])
 
 submodule.controller('ProductsController', [function() {
 
 }]);
-a
