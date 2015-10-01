2fc4a8a106359926f957c6e69aa98282e91318b2
20LDiegoM <luisdiego.m20@gmail.com>
2015-10-01 17:11:42 -0600

    Subir todo

---
+++ 03 -Controllers/js/controllers.js
@@ -0,0 +1,25 @@
+var submodule = angular.module('bootstrap.controllers', []);
+
+submodule.controller('HomeController', ['$scope', function($scope) {
+  $scope.title = 'Hello World';
+  $scope.promotions = [{
+    name: 'Coca Cola',
+    price: '$2'
+  }, {
+    name: 'Pepsi',
+    price: '$3'
+  }, {
+    name: 'Red Bull',
+    price: '$4'
+  }];
+
+  $scope.add = function() {
+    var newPromotion = angular.copy($scope.newPromotion);
+    $scope.promotions.push(newPromotion);
+  };
+
+}])
+
+submodule.controller('ProductsController', [function() {
+
+}]);
